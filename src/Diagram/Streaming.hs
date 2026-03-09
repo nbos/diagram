ac1d49706ddf24d4f538aa0085c5c68b8dbc0b57
nbos <35620093+nbos@users.noreply.github.com>
2026-03-09 10:09:59 -0400

    PrimMonad instance for Stream

---
+++ src/Diagram/Streaming.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+module Diagram.Streaming (module Diagram.Streaming) where
+
+import Control.Monad.Primitive (PrimMonad(..))
+import Streaming (MonadTrans(lift), Of, Stream)
+import qualified Streaming.Prelude as S
+import Data.Maybe (isJust)
+import Diagram.Util ((.:))
+
+instance PrimMonad m => PrimMonad (Stream (Of a) m) where
+  type PrimState (Stream (Of a) m) = PrimState m
+  primitive = lift . primitive
+
+-- | Return the return values of the streams if they are equal, Nothing
+-- if they are not
+eq :: (Monad m, Eq a) =>
+      Stream (Of a) m r -> Stream (Of a) m s -> m (Maybe (r,s))
+eq str0 str1 = do
+  ea0 <- S.next str0
+  ea1 <- S.next str1
+  case (ea0,ea1) of
+    (Left r, Left s) -> return $ Just (r,s)
+    (Right (a0,str0'), Right (a1,str1')) | a0 == a1 -> eq str0' str1'
+                                       | otherwise -> return Nothing
+    (_, _) -> return Nothing
+
+-- | True if the streams contain equal sequences of values
+eq_ :: (Monad m, Eq a) => Stream (Of a) m r -> Stream (Of a) m s -> m Bool
+eq_ = fmap isJust .: eq
