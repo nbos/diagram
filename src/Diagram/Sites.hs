cf49d9466bb07a9cb0cee1bb312ba9b4e75cd58a
nbos <35620093+nbos@users.noreply.github.com>
2026-03-31 11:59:29 -0400

    Composable (join) struct for Sites

--- src/Diagram/Sites.hs
+++ src/Diagram/Sites.hs
@@ -4,126 +4,67 @@
 module Diagram.Sites (module Diagram.Sites) where
 
 import Control.Monad
-import Control.Monad.Extra
 import Control.Lens hiding (Index)
-import Control.Monad.Primitive (PrimMonad(PrimState))
 import Control.Monad.State.Strict
 
-import Data.IntSet (IntSet)
-import qualified Data.IntSet as IS
+import Data.IntMap (IntMap)
+import qualified Data.IntMap as IM
 import Data.Strict.Tuple (Pair((:!:)),(:!:))
 
 import Diagram.UnionType (Sym)
-import Diagram.Joints (Doubly)
-import Diagram.JointType (JointType)
-import qualified Diagram.JointType as JT
 import Diagram.Doubly (Index)
-import qualified Diagram.Doubly as D
 
 import Diagram.Util
 
+type Len = Int
 data Sites = Sites
-  { _jointType      :: !JointType
-  , _runHeads       :: !IntSet
-  , _bySndConstr    :: !IntSet }
+  { _counts :: !(IntMap Int) -- :: s --> n
+  , _heads  :: !(IntMap (Len :!: (Index, Sym))) -- :: hd --> (len, (tl,stl))
+  , _tails  :: !(IntMap (Len :!: Index)) }      -- :: tl --> (len, hd)
   deriving(Show,Eq)
 makeLenses ''Sites
 
-join :: forall m. PrimMonad m => Doubly (PrimState m) -> Sites -> Sites -> m Sites
-join ss sitesA sitesB = do
-  sitesA' :!: sitesB' <- flip execStateT (sitesA :!: sitesB) $ do
-    forM_ (IS.elems brokenAs) $ assertNext >=> goBroken _2 _1
-    forM_ (IS.elems brokenBs) $ assertNext >=> goBroken _1 _2
-
-  undefined
+join :: Sites -> Sites -> Sites
+join sitesA sitesB = runIdentity $ flip evalStateT (sitesA :!: sitesB) $ do
+  ns <- uses2 (_1.counts) (_2.counts) $ IM.unionWith (+)
+  cAB <- uses2 (_1.tails) (_2.heads) $ IM.intersectionWith (,)
+  ns' <- goSeam (IM.toList cAB) ns
+  cBA <- uses2 (_2.tails) (_1.heads) $ IM.intersectionWith (,)
+  ns'' <- goSeam (IM.toList cBA) ns'
+  hds <- uses2 (_1.heads) (_2.heads) $ IM.unionWithKey err
+  tls <- uses2 (_1.tails) (_2.tails) $ IM.unionWithKey err
+  return $ Sites ns'' hds tls
 
   where
-    -- Given lenses into the breaking and broken sides, treat the
-    -- breaking of a joint at (i0,i1) from a joint at (im1,i0), given
-    -- only index i1.
-    goBroken :: Lens' (Sites :!: Sites) Sites -> Lens' (Sites :!: Sites) Sites ->
-                Index -> StateT (Sites :!: Sites) m ()
-    goBroken breaking broken = go
-      where
-        go :: Index -> StateT (Sites :!: Sites) m ()
-        go i1 = do
-          broken.bySndConstr %= IS.delete i1
-          whenJustM (next i1) $ \i2 -> do
-            s1 <- read i1
-            s2 <- read i2
-            moreBreaking <- (breaking.jointType) `uses` JT.member (s1,s2)
-            if not moreBreaking then zoom broken $ goNonConstr (i1,s1) (i2,s2)
-              else whenJustM (next i2) $ \i3 ->
-              do s3 <- read i3
-                 moreBroken <- (broken.jointType) `uses` JT.member (s2,s3)
-                 when moreBroken $ go i3 -- cont.
-
-    -- Upon the breaking of a joint at (i0,i1), at the end of a breaking
-    -- run (i.e. no joint at (i1,i2) on the breaking side) repeatedly
-    -- check if the freed i1 allows for a previously non-constructive
-    -- joint at (i1,i2) to become constructive on the broken side,
-    -- taking precedence from a possibly empty tail on the broken side.
-    goNonConstr :: (Index,Sym) -> (Index,Sym) -> StateT Sites m ()
-    goNonConstr (i1,s1) (i2,s2) = do
-      nonConstr <- jointType `uses` JT.member (s1,s2)
-
-      undefined
-
-    Sites jtA hdsA bySndA = sitesA
-    Sites jtB hdsB bySndB = sitesB
-
-    safeHdsA = hdsA IS.\\ bySndB
-    brokenAs = hdsA `IS.intersection` bySndB
-
-    safeHdsB = hdsB IS.\\ bySndA
-    brokenBs = hdsB `IS.intersection` bySndA
-
-    read :: MonadTrans t => Index -> t m Sym
-    read = lift . D.read ss
-    prev :: MonadTrans t => Index -> t m (Maybe Index)
-    prev = lift . D.prev ss
-    next :: MonadTrans t => Index -> t m (Maybe Index)
-    next = lift . D.next ss
-    assertNext :: MonadTrans t => Index -> t m Index
-    assertNext = lift . D.unsafeNext ss
+    goSeam :: [(Index, (Len :!: Index, Len :!: (Index, Sym)))] -> IntMap Int ->
+              StateT (Sites :!: Sites) Identity (IntMap Int)
+    goSeam = flip $ foldM $ \dns (i, (lenA :!: hdA, lenB :!: (tlB,stl))) -> do
+      let lenA' = lenA + lenB - 1
+      _1.heads %= IM.insert hdA (lenA' :!: (tlB,stl))
+      _1.tails %= IM.delete i
+      _2.heads %= IM.delete i
+      _2.tails %= IM.insert tlB (lenA' :!: hdA)
+      let oldTailB | even lenB = 1
+                   | otherwise = 0
+          newTailB | even lenA' = 1
+                   | otherwise = 0
+          deltaTailB = newTailB - oldTailB
+      if deltaTailB == 0 then return dns
+        else return $ flip2 IM.alter stl dns $ \case
+        Nothing  -> Just deltaTailB
+        Just ntl -> nothingIf (== 0) (ntl + deltaTailB)
 
     err :: (Show k, Show v0, Show v1) => k -> v0 -> v1 -> a
     err = error . ("Sites.join: collision: " ++) . show .:. (,,)
 
--- NOTE: For a string of symbols and a joint construction rule which is
---   a function on pairs of symbols (e.g. a decision (s0,s1) -> {True,
---   False}), for each symbol to be part of at most 1 construction,
---   giving the arbitrary precedence to the first joint when a conflict
---   arises, some joints are filtered out (marked non-constructive) on
---   the grounds of their first symbol belongs to the preceeding joint
---   (in the second position).
---
---   When it comes to merging two sets of constructions, the conflicts
---   between joints needs to be re-computed, making some previously
---   constructive joints non-constructive and vice versa.
---
---   We can simplify the computation of conflicts by representing
---   constructive joints of as "runs" of consecutive (non-overlapping)
---   constructions. Because there are no gaps in a run for a joint to
---   squeeze in, if the head of a run is *not* "broken" by a joint
---   appearing right before it, none of the joints in the run will be
---   broken either. If a head *is* broken by a joint, then so are the
---   `n` following joints in that run for a breaking joint with `n` more
---   joints in its run.
---
---   When a head (and a possibly empty set of following joints) is
---   broken by the tail of a new head, it may free a previously
---   unconstructable run, which, by being in off-step with the now
---   broken head of the run, are in sync with the breaking run and
---   appends to it. The run with the broken head only restores a head
---   for itself after both the tail of the breaking run and the
---   previously unconstructable run end, if there are any joints left at
---   that point.
---
---   Some runs in different sets will concatenate without any breaking
---   occuring, simply by being in sync and because one is terminating
---   immediately before the second one begins.
---
---   With this representation, we avoid arbitrarily long chains of
---   lookups to resolve a chain of conflicts because it is impossible
---   for a head that is invalidated/broken--
+
+-- | Use the target of two lenses in the current state with a function
+uses2 :: MonadState s m => Lens' s a -> Lens' s b -> (a -> b -> c) -> m c
+uses2 a b = uses a >=> uses b
+{-# INLINE uses2 #-}
+
+-- | Use the target of three lenses in the current state with a function
+uses3 :: MonadState s m =>
+  Lens' s a -> Lens' s b -> Lens' s c -> (a -> b -> c -> d) -> m d
+uses3 a b c = uses a >=> uses b >=> uses c
+{-# INLINE uses3 #-}
